a13f32a7a18bed5c0b3095bc06f68408c3b4c32f
licht <licht.guilherme@gmail.com>
2021-09-19 16:34:30 -0400

    first commit: back-end and front-end folders setup

---
+++ psicoworking/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>PSICOWORKING HOME PAGE</h2>
+      <h3>front-end</h3>
+    </div>
+  );
+}
+
+export default App;

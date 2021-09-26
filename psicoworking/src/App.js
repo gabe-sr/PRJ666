97a7da762ce13461cb3642ad8fc05fc41e87dd56
licht <licht.guilherme@gmail.com>
2021-09-26 13:03:56 -0400

    Added custom navbar, react-router, started signup page

--- psicoworking/src/App.js
+++ psicoworking/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
-import MainPage from './MainPage';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Navbar
+import NavbarTemplate from "./common/navbar/NavbarTemplate";
+import { navItems } from "./main_page/navbar_data";
+
+// Routes
+import MainPage from "./main_page/MainPage";
+import SignUpModal from "./sign_up/SignUpModal";
+
+// Main app
 function App() {
   return (
     <div className="App">
-      <MainPage/>
+      <Router>
+        <NavbarTemplate navData={navItems} />
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route path="/about"></Route>
+          <Route path="/pricing"></Route>
+          <Route path="/signup">
+            <SignUpModal />
+          </Route>
+          <Route path="/login"></Route>
+          {/* <Route path="/person/:id" children={<Person />}></Route>
+          <Route path="*">
+            <Error />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }

87c1c7e587b1b7254bf29751085d74ed89491673
licht <licht.guilherme@gmail.com>
2021-09-28 11:11:09 -0400

    finished sign up form design

--- psicoworking/src/Components/sign_up/sign_up-form/form-components/TextField.js
+++ psicoworking/src/Components/sign_up/sign_up-form/form-components/TextField.js
@@ -4,7 +4,7 @@ import { ErrorMessage, useField } from "formik";
 export const TextField = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <div className="mb-2">
+    <div className="mb-3">
       <label htmlFor={field.name}>{label}</label>
       <input
         className={`form-control shadow-none ${
